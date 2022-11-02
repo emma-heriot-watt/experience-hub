2378cfad43aac58b3d805e052e2c78b7868f97a3
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-11-02 09:30:18 +0000

    build: add step to install unzip

--- scripts/setup-ec2-instance.sh
+++ scripts/setup-ec2-instance.sh
@@ -81,6 +81,7 @@ sudo apt update -y &&
 	sudo systemctl restart docker
 
 # ------------------------------ Install AWS CLI ----------------------------- #
+sudo apt install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
