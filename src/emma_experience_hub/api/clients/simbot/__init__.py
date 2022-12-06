0f5bbc2a3d58a6847bb23867afb546d38b76daa5
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-12-06 10:43:04 +0000

    fix: remove the placeholder client from the `__init__.py`

--- src/emma_experience_hub/api/clients/simbot/__init__.py
+++ src/emma_experience_hub/api/clients/simbot/__init__.py
@@ -6,7 +6,6 @@ from emma_experience_hub.api.clients.simbot.cache import (
     SimBotPydanticCacheClient,
 )
 from emma_experience_hub.api.clients.simbot.features import SimBotFeaturesClient
-from emma_experience_hub.api.clients.simbot.placeholder_vision import PlaceholderVisionClient
 from emma_experience_hub.api.clients.simbot.session_db import SimBotSessionDbClient
 from emma_experience_hub.api.clients.simbot.utterance_generator import (
     SimBotUtteranceGeneratorClient,
