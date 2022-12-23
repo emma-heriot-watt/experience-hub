070a0c272ba315448077772828d06410ea1449c8
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-12-23 15:10:40 +0000

    fix: optimise the compress segmentation mask fn (#239)
    Alexa people's is faster. We are just using theirs. Am numpty.
    
    ---
    
    - [ ] This PR **has** been tested in the staging environment.
    - [ ] This PR altered the decision-making process of the bot and these
    alternations have been included in the [Figma
    diagram](https://www.figma.com/file/lHucvo4KDMaex98TfgYbNq/Agent-Decision-Making-Process-Implementation?node-id=0%3A1&t=mzKjVjRXdeBty4co-1).

--- src/emma_experience_hub/functions/simbot/masks.py
+++ src/emma_experience_hub/functions/simbot/masks.py
@@ -3,62 +3,26 @@ import torch
 from emma_experience_hub.datamodels.simbot.payloads import SimBotObjectMaskType
 
 
-class CompressSegmentationMask:
-    """Refactored version of their compress mask function.
-
-    Just to make a bit more sense of it.
-    """
-
-    __slots__ = ("run_idx", "run_length", "compressed_mask", "current_run")
-
-    def __init__(self) -> None:
-        self.run_idx = 0
-        self.run_length = 0
-        self.compressed_mask: SimBotObjectMaskType = []
-        self.current_run = False
-
-    def __call__(self, mask: torch.Tensor) -> SimBotObjectMaskType:
-        """Compress the mask."""
-        self.reset()
-
-        for x_idx in range(mask.size(0)):
-            for y_idx in range(mask.size(1)):
-                self.step(mask, x_idx, y_idx)
-
-        return self.compressed_mask
-
-    def reset(self) -> None:
-        """Reset the state of the class."""
-        self.run_idx = 0
-        self.run_length = 0
-        self.compressed_mask = []
-        self.current_run = False
-
-    def step(self, mask: torch.Tensor, x_idx: int, y_idx: int) -> None:
-        """Take step in loop."""
-        if mask[x_idx][y_idx] == 1 and not self.current_run:
-            self.start_new_run()
-
-        if mask[x_idx][y_idx] == 0 and self.current_run:
-            self.end_current_run()
-
-        if self.current_run:
-            self.run_length += 1
-
-        self.run_idx += 1
-
-    def start_new_run(self) -> None:
-        """Start a new run to compress."""
-        self.current_run = True
-        self.compressed_mask.append([self.run_idx, 0])
-
-    def end_current_run(self) -> None:
-        """End the current run."""
-        self.current_run = False
-        self.compressed_mask[-1][1] = self.run_length
-        self.run_length = 0
-
-
-def compress_segmentation_mask(mask: torch.Tensor) -> SimBotObjectMaskType:
+def compress_segmentation_mask(mask: torch.Tensor) -> SimBotObjectMaskType:  # noqa: WPS231
     """Compress the segmenmtation mask for the arena."""
-    return CompressSegmentationMask()(mask)
+    run_len_compressed: SimBotObjectMaskType = []
+    idx = 0
+    curr_run = False
+    run_len = 0
+
+    for x_idx, _ in enumerate(mask):
+        for y_idx, _ in enumerate(mask[x_idx]):
+            if mask[x_idx][y_idx] == 1 and not curr_run:
+                curr_run = True
+                run_len_compressed.append([idx, 0])
+            if mask[x_idx][y_idx] == 0 and curr_run:
+                curr_run = False
+                run_len_compressed[-1][1] = run_len
+                run_len = 0
+            if curr_run:
+                run_len += 1
+            idx += 1
+    if curr_run:
+        run_len_compressed[-1][1] = run_len
+
+    return run_len_compressed
