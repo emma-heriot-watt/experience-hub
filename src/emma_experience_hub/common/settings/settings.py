62cedddf9cb6cc7412f53e45bb341afc05fec9ea
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-01-01 17:33:54 +0000

    refactor: remove old commented out code from settings

--- src/emma_experience_hub/common/settings/settings.py
+++ src/emma_experience_hub/common/settings/settings.py
@@ -2,24 +2,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    """Environment variables which will be needed.
+    """Environment variables which will be needed."""
 
-    This class validates the environment variables to ensure they exist.
-    """
-
-    # github_pat: SecretStr
     log_level: str = "debug"
 
-    # remote_perception_checkpoint_uri: AnyUrl
-    # remote_policy_checkpoint_uri: AnyUrl
     cuda_version_upper_bound: float = 11.3
-
-    # Repository directories
-    # perception_repo_root: DirectoryPath
-    # perception_python_executable: FilePath
-
-    # policy_repo_root: DirectoryPath
-    # policy_python_executable: FilePath
 
     class Config:
         """Config for the Settings."""
