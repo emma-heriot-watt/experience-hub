b216cd84220b887b42379a54c5e23235250eb35a
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-02-10 12:04:28 +0000

    chore: update setup ec2 instance script

--- scripts/setup-ec2-instance.sh
+++ scripts/setup-ec2-instance.sh
@@ -10,7 +10,9 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 	sudo apt update &&
 	sudo apt install gh -y
 
-gh auth login --with-token "${GITHUB_PAT}"
+echo "$GITHUB_PAT" | gh auth login --with-token
+gh auth setup-git
+gh auth status
 
 # ---------------------- Install pyenv for python sanity --------------------- #
 curl https://pyenv.run | bash
@@ -32,14 +34,14 @@ curl -sSL https://install.python-poetry.org | python3 -
 echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >>~/.bashrc
 
 # Create venvs within the project
-poetry config virtualenvs.in-project true
+/home/ubuntu/.local/bin/poetry config virtualenvs.in-project true
 
 # Handle temporary poetry issue
 # https://github.com/python-poetry/poetry/issues/1917#issuecomment-1251667047
 echo 'export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring' >>~/.bashrc
 
 # Add poethepoet for all the poetry hooks
-poetry self add 'poethepoet[poetry_plugin]'
+/home/ubuntu/.local/bin/poetry self add 'poethepoet[poetry_plugin]'
 
 # --------------------- Install Python build dependencies -------------------- #
 sudo apt update -y &&
@@ -51,12 +53,16 @@ sudo apt update -y &&
 echo 'set -g default-terminal "screen-256color"' >>~/.tmux.conf
 
 # ------------------------------- Install CUDA ------------------------------- #
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin &&
-	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600 &&
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub &&
-	sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /" &&
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb &&
+	sudo dpkg -i cuda-keyring_1.0-1_all.deb &&
 	sudo apt update -y &&
-	sudo apt install -y cuda
+	sudo apt install -y cuda-11-8
+
+# shellcheck disable=SC2016
+{
+	echo 'export PATH=/usr/local/cuda-11.8/bin${PATH:+:${PATH}}'
+	echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}'
+} >>~/.bashrc
 
 # ------------------------------ Install Docker ------------------------------ #
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
