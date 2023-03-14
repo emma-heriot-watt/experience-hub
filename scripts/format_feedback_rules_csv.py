90a64da5c160f26344e71192b985bb5dfdc5c76f
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-03-14 10:22:44 +0000

    style: add script to format the feedback rules by grouping them by their conditions (#443)
    This doesn't really do anything within the underlying code apart from
    formatting the rules into chunks. 
    
    No rules are lost and the the automatic formatter script can be run as
    many times as needed.
    
    Since all the tests still pass, adding blank lines to the CSV is not
    breaking our loading of the rules
    
    ---
    
    - [ ] This PR **has** been tested in the staging environment.
    - [ ] This PR altered the decision-making process of the bot and these
    alterations have been included in the [Figma
    diagram](https://www.figma.com/file/lHucvo4KDMaex98TfgYbNq/Agent-Decision-Making-Process-Implementation?node-id=0%3A1&t=mzKjVjRXdeBty4co-1).

---
+++ scripts/format_feedback_rules_csv.py
@@ -0,0 +1,44 @@
+import itertools
+from pathlib import Path
+
+from loguru import logger
+
+
+if __name__ == "__main__":
+    # Get the path to the CSV file
+    feedback_csv = Path("src/emma_experience_hub/constants/simbot/feedback_rules.csv")
+
+    # Load all the lines from the CSV
+    all_lines_from_csv: list[str] = [line for line in feedback_csv.read_text().split("\n") if line]
+
+    header_row = all_lines_from_csv[0]
+    rule_rows = all_lines_from_csv[1:]
+
+    logger.info(f"Loaded {len(rule_rows)} rules from the CSV")
+
+    # Sort all the rules by their condition
+    sorted_rule_rows = sorted([line.split(",") for line in rule_rows], key=lambda row: row[0])
+
+    # Group lines by their condition
+    rule_groups = [
+        [",".join(line) for line in lines]
+        for _, lines in itertools.groupby(sorted_rule_rows, lambda line: line[0])
+    ]
+
+    logger.info(f"Total {len(rule_groups)} groupings of rules")
+
+    # Write all the rules to the file with spaces between them
+    with open(feedback_csv, "w") as feedback_csv_file:
+        # Add the header row in
+        feedback_csv_file.write(header_row)
+        feedback_csv_file.write("\n")
+
+        for rule_group in rule_groups:
+            for rule in rule_group:
+                feedback_csv_file.write(rule)
+                feedback_csv_file.write("\n")
+
+            # Add a blank line after each chunk of rules
+            feedback_csv_file.write("\n")
+
+    logger.info("Done!")
